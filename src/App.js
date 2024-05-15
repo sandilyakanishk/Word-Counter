3c55c8e637d56e724ac32342abd824d8b4c5920e
Kanishk sandilya <sandilyakanishk@gmail.com>
2024-05-16 01:13:11 +0530

    useState & Style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextFom from './components/TextFom';
+// import TextFom from './components/TextFom';
 
 function App() {
   return (
   <>
   <Navbar title = "TextUtils" aboutText = "Aboutus" />
-  <div className="container my-5">
+  {/* <div className="container my-5">
   <TextFom heading = "Enter Your Text Here" />
+  </div> */}
+  <div className="container">
+    <About/>
   </div>
   </>
   );
